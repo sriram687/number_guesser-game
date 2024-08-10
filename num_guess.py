45901b1c418d7807f309219e5eed9aac3dec2685
pikachu <msram2006@gmail.com>
2024-08-10 13:46:53 +0530

    game has been done

--- num_guess.py
+++ num_guess.py
@@ -13,4 +13,23 @@ else:
     quit()
 
 random_number = random.randint(0, top_of_range)
-print(random_number)
+guesses = 0
+
+while True:
+    guesses += 1
+    user_guess = input("Make a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print('Please type a number next time.')
+        continue
+
+    if user_guess == random_number:
+        print("You got it!")
+        break
+    elif user_guess > random_number:
+        print("You were above the number!")
+    else:
+        print("You were below the number!")
+
+print("You got it in", guesses, "guesses")
